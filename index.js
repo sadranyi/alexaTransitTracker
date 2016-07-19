1673e7519f379ee1fe79a8a761f7c7f277829df9
sadranyi <sadranyi@gmail.com>
2016-07-18 17:22:35 -0700

    Added moment package to help with times

--- index.js
+++ index.js
@@ -2,13 +2,20 @@ var http = require('http'),
     AlexaSkill = require('./AlexaSkill'),
     APP_ID = 'amzn1.ask.skill.ec8d058d-0c81-4645-88be-f5a5b51c9b31',
     TRIMET_KEY = 'C40112249CB0B576572DE9666',
-    xml2js = require('xml2js'),
-    express = require('express');
+    express = require('express'),
+    moment = require('moment');
 
 var app = express();
 
 var url = function (stopId){
     return 'http://developer.trimet.org/ws/v2/arrivals?locIDs=' + stopId + '&appID=' + TRIMET_KEY;
+}
+
+var timeToArrival = function(scheduledtime){
+    var now = moment();
+    var scheduled = moment(scheduledtime);
+
+    return now.to(scheduled);
 }
 
 var getArrival = function(stopId, callback){
@@ -28,12 +35,36 @@ var getArrival = function(stopId, callback){
 }
 
 var myCallback = function(data){
-    console.log(data);
+    var jsdata = JSON.parse(data);
+    var hasArrivals = jsdata.resultSet.arrival.length;
+    
+    if(hasArrivals > 0){
+        var location = jsdata.resultSet.location[0];
+        var arrivals = jsdata.resultSet.arrival;
+
+        console.log("Arrivals for " + location.desc + ".");
+        arrivals.forEach(function(arr) {
+            console.log("Bus " + arr.shortSign + " scheduled " + moment(arr.scheduled).calendar(arr.scheduled) + ", estimated arrival " + timeToArrival(arr.scheduled) + ".");
+        });
+    }
 }
 
 var handleNexArrivalRequest = function(intent, session, response){
     getArrival(intent.slots.stopId.value, function(data){
-       
+
+        var jsdata = JSON.parse(data);
+        var hasArrivals = jsdata.resultSet.arrival.length;
+
+        if(hasArrivals > 0){
+            var location = jsdata.resultSet.location[0];
+            var arrivals = jsdata.resultSet.arrival;
+
+            console.log("Arrivals for " + location.desc + ".");
+
+            arrivals.forEach(function(arr) {
+                console.log("Bus " + arr.shortSign + " scheduled " + moment(arr.scheduled).calendar(arr.scheduled) + ", estimated arrival " + timeToArrival(arr.scheduled) + ".");
+            });
+        }
     })
 }
 
