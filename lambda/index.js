fa25b2cdca367e8229c5fb9adc5c2055ddf7e020
sadranyi <sadranyi@gmail.com>
2016-07-20 12:37:36 -0700

    added helper pacakges and folder structure

--- lambda/index.js
+++ lambda/index.js
@@ -4,7 +4,8 @@ var http = require('http'),
     TRIMET_KEY = 'C40112249CB0B576572DE9666',
     express = require('express'),
     moment = require('moment'),
-    mtz = require('moment-timezone');
+    mtz = require('moment-timezone'),
+    util = require('util-is');
 
 
 var app = express();
@@ -12,6 +13,8 @@ var app = express();
 var cardText;
 var cardHeading;
 var noArrivalText;
+var speechText;
+var repromtText;
 
 var url = function (stopId){
     return 'http://developer.trimet.org/ws/v2/arrivals?locIDs=' + stopId + '&appID=' + TRIMET_KEY;
@@ -46,11 +49,14 @@ var replaceSpeech = function(textVal, searchval, replaceVal){
 
 /** LOCAL DEBUG
  * COMMENT BEFORE DEPLOY
+
 var myCallback = function(data){
+
     var jsdata = JSON.parse(data);
-    var hasArrivals = jsdata.resultSet.arrival.length;
-    
-    if(hasArrivals > 0){
+    var hasArrivals = jsdata.resultSet.arrival && jsdata.resultSet.arrival.length > 0;
+    var hasErrors = jsdata.resultSet.error ? true : false;
+
+    if(hasArrivals){
         var location = jsdata.resultSet.location[0];
         var arrivals = jsdata.resultSet.arrival;
 
@@ -59,16 +65,23 @@ var myCallback = function(data){
             console.log("Bus " + arr.shortSign + ", scheduled " + moment(arr.scheduled).calendar(arr.scheduled) + ", estimated arrival " + timeToArrival(arr.scheduled) + ".");
         });
     }
+    else if(hasErrors){
+        console.log(jsdata.resultSet.error.content);
+    }
+    else{
+        console.log("the stop ID you requested is invalid. Please say a valid stop ID!");
+    }
 };
- */
+*/
 
 var handleNexArrivalRequest = function(intent, session, response){
     getArrival(intent.slots.stopId.value, function(data){
 
         var jsdata = JSON.parse(data);
-        var hasArrivals = jsdata.resultSet.arrival.length;
+        var hasArrivals = jsdata.resultSet.arrival && jsdata.resultSet.arrival.length > 0;
+        var hasErrors = jsdata.resultSet.error ? true : false;
 
-        if(hasArrivals > 0){
+        if(hasArrivals){
             var location = jsdata.resultSet.location[0];
             var arrivals = jsdata.resultSet.arrival;
 
@@ -76,12 +89,14 @@ var handleNexArrivalRequest = function(intent, session, response){
             arrivals.forEach(function(arr) {
                 cardText += "Bus " + replaceSpeech(arr.shortSign, "TC", "Transit Center") + ", scheduled at " + mtz(arr.scheduled).tz('America/Vancouver').format("h mm a") + ", estimated arrival " + timeToArrival(arr.scheduled) + ". ";
             });
-            
         }
-        else
-        {
-            noArrivalText = 'No arrivals for stop: ' + intent.slots.stopId.value;
-            cardText = text;
+        else if(hasErrors){
+            errorText = 'The Stop ID you requested is invalid. Please say a valid stop ID!';
+            cardText = errorText;
+        }
+        else{
+            noStopIdText = 'The Stop ID you requested is invalid. Please say a valid stop ID!';
+            cardText = noStopIdText;
         }
 
         cardHeading = 'Next arrival for stop ID: ' + intent.slots.stopId.value;
@@ -94,15 +109,20 @@ var ArrivalSchedule = function(){
 };
 
 ArrivalSchedule.prototype = Object.create(AlexaSkill.prototype);
-ArrivalSchedule.prototype.constructor =ArrivalSchedule;
+ArrivalSchedule.prototype.constructor = ArrivalSchedule;
 
 /** EVENTS */
-ArrivalSchedule.prototype.eventHandlers.onLaunch = function(launchRequest, session, response){
-    var outputText = 'Welcome to Trimet Transit Tracker. ' +
-    'Say a stop ID and i will tell you the arrival times.';
+ArrivalSchedule.prototype.eventHandlers.onSessionStarted = function (sessionStartedRequest, session) {
+    console.log("PDX Bus Tracker onSessionStarted requestId: " + sessionStartedRequest.requestId + ", sessionId: " + session.sessionId);
+};
 
-    var repromtText = 'Which stop ID do you want arrival times for ?';
-    response.ask(outputText, repromtText);
+ArrivalSchedule.prototype.eventHandlers.onSessionEnded = function (sessionEndedRequest, session) {
+    console.log("PDX Bus Tracker onSessionEnded requestId: " + sessionEndedRequest.requestId + ", sessionId: " + session.sessionId);
+};
+
+ArrivalSchedule.prototype.eventHandlers.onLaunch = function(launchRequest, session, response){
+    console.log("PDX Bus Tracker onLaunch requestId: " + launchRequest.requestId + ", sessionId: " + session.sessionId);
+    getWelcomeMessage(response);
 };
 
 ArrivalSchedule.prototype.intentHandlers = {
@@ -110,12 +130,36 @@ ArrivalSchedule.prototype.intentHandlers = {
         handleNexArrivalRequest(intent, session, response);
     },
 
-    HelpIntent: function(intent, session, response){
-        var speachOutput = "Get the Next arrivals for Trimet Stop ID. " + 
-        "Which Stop would you want arrivals for?";
-        response.ask(speachOutput);
+    "AMAZON.HelpIntent": function(intent, session, response){
+        var speechOutText = "You can ask for bus arrivals and schedules at a given stop ID. " + 
+        "For example, two two six six, or you can say exit. " + 
+        "Now, which Stop would you want arrivals for?";
+
+        var repromtOutText = "I am sorry I did not understand that. " + 
+        "You can say two two six six, Or you can say exit. " + 
+        "Now, which Stop would you want arrivals for?"
+
+        response.ask(speechOutText, repromtOutText);
+    },
+
+    "AMAZON.StopIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
+    },
+
+    "AMAZON.CancelIntent": function (intent, session, response) {
+        var speechOutput = "Goodbye";
+        response.tell(speechOutput);
     }
 };
+
+var getWelcomeMessage = function (response) {
+    speechText = 'Welcome to Trimet Transit Tracker. ' +
+    'Say a stop ID and I will tell you the arrival times.';
+
+    repromtText = 'Which stop ID do you want arrival times for ?';
+    response.ask(outputText, repromtText);
+}
 
 exports.handler = function(event, context){
     var skill = new ArrivalSchedule();
@@ -124,14 +168,13 @@ exports.handler = function(event, context){
 
 /** LOCAL DEBUB 
  *  REMOVE BEFORE DEPLOYMENT 
- 
 app.get('/alexa', function(req, res){
     console.log("We are game");
     res.end('welcome to Trimet Transit Tracker on Alexa');
-    getArrival(2276, myCallback);
+    getArrival(2, myCallback);
 });
 
 var server = app.listen(9290, function(){
-    console.log("Test Server running .....");
+    console.log("Test Server running @ 9290");
 });
 */
